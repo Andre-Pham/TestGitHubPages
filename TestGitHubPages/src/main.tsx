77001c9e43ae82e3a0241d1647ca25b4ec202992
Andre Pham <andrepham33@gmail.com>
2024-05-07 23:59:33 +1000

    Setup basic hosting

--- TestGitHubPages/src/main.tsx
+++ TestGitHubPages/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Education from "./Education.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/education",
+//     element: <Education />
+//   }
+// ], {
+//   basename: "/TestGitHubPages",
+// })
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/education" element={<Education />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+);
